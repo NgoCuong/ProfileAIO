1490cd947f9b8e3e8e740471ac3a4f45104dc63d
ngocuong <cuonngo93@gmail.com>
2018-09-25 21:16:42 -0700

    test API endpoints

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-//var mongodb = require("mongodb");
-//var ObjectID = mongodb.ObjectID;
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-//var CONTACTS_COLLECTION = "contacts";
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(bodyParser.json());
@@ -12,35 +12,35 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-app.use('*', (req, res, next) => {
-  res.status(200).json({ success: false, message: 'Does not match any resource of the routing.' });
-});
+// app.use('*', (req, res, next) => {
+//   res.status(200).json({ success: false, message: 'Does not match any resource of the routing.' });
+// });
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-//var db;
+var db;
 
 // Connect to the database before starting the application server.
-// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
+ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+   if (err) {
+     console.log(err);
+     process.exit(1);
+   }
 
 //   // Save database object from the callback for reuse.
-//   db = client.db();
-//   console.log("Database connection ready");
+   db = client.db();
+   console.log("Database connection ready");
 
 //   // Initialize the app.
-//   var server = app.listen(process.env.PORT || 8080, function () {
-//     var port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
+   var server = app.listen(process.env.PORT || 8080, function () {
+     var port = server.address().port;
+     console.log("App now running on port", port);
+   });
+ });
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+// var server = app.listen(process.env.PORT || 8080, function () {
+//   var port = server.address().port;
+//   console.log("App now running on port", port);
+// });
 
 
 
@@ -48,14 +48,53 @@ var server = app.listen(process.env.PORT || 8080, function () {
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
   }
+});
 
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
 
-   
-  
-  app.get('/hello', (req, res) => {
-    let result = 'helloWorld. Testing GET REQUEST'
-    res.send(result)
-  })
+app.get("/api/contacts/:id", function(req, res) {
+});
+
+app.put("/api/contacts/:id", function(req, res) {
+});
+
+app.delete("/api/contacts/:id", function(req, res) {
+});
