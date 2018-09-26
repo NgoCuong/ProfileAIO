1c32c5d23e2b64c67c5885b2a95d474bcaf1dd10
ngocuong <cuonngo93@gmail.com>
2018-09-25 22:19:43 -0700

    fixing routes

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.static(distDir));
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
 
-  res.sendFile(path.join(__dirname, 'index.html'));
+  console.log(path.join(distDir, 'index.html'));
+  res.sendFile(path.join(distDir, 'index.html'));
 });
 
 // app.use('*', (req, res, next) => {
