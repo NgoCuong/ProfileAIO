2ea9d11396d658924a4800f5052a440f3111ad32
ngocuong <cuonngo93@gmail.com>
2018-09-27 19:57:35 -0700

    register post request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { ProfileService } from './-services/profile.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [UserService, ProfileService],
   bootstrap: [AppComponent]
