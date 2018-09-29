a6409249f8b2f822286602eca514b1d394f54c69
ngocuong <cuonngo93@gmail.com>
2018-09-28 20:52:50 -0700

    refactor #2

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ var server = app.listen(process.env.PORT || 8080, function () {
 
 
 // routes
-// var userRoutes = require("./server/controllers/user");
-// app.use("/user", userRoutes);
+var userRoutes = require("./server/controllers/user");
+app.use("/user", userRoutes);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
