6801f7fed702eb5094f8f00e2dc7475789bbae5f
ngocuong <cuonngo93@gmail.com>
2018-09-29 00:01:53 -0700

    refactor route

---
+++ server/routes/user.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router;
+var User = require("../models/user");
+
+router.get("/api/contacts", function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(users);
+        }
+    })
+});
+
+router.post("/api/contacts", function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var date = new Date();
+
+    var newUser = { email: email, password: password, date: date };
+
+    User.create(newUser, function (err, user) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(user);
+        }
+    });
+});
