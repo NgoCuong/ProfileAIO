e0ab2697140f18b70fa0fd293fda714edbbdaa49
ngocuong <cuonngo93@gmail.com>
2018-09-28 22:25:18 -0700

    refactor#3

--- server.js
+++ server.js
@@ -10,38 +10,29 @@ app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
 
-var mongoURI = "mongodb://localhost:27017/test";
-var MongoDB = mongoose.connect(mongoURI).connection;
-MongoDB.on('error', function(err) { console.log(err.message); });
-MongoDB.once('open', function() {
-  console.log("mongodb connection open");
+var http = require ('http');         // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 27017;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
 });
-
-
-
-// var http = require ('http');         // For serving a basic web page.
-// var mongoose = require ("mongoose"); // The reason for this demo.
-
-// // Here we find an appropriate database to connect to, defaulting to
-// // localhost if we don't find one.
-// var uristring =
-// process.env.MONGOLAB_URI ||
-// process.env.MONGOHQ_URL ||
-// 'mongodb://localhost/HelloMongoose';
-
-// // The http server will listen to an appropriate port, or default to
-// // port 5000.
-// var theport = process.env.PORT || 5000;
-
-// // Makes connection asynchronously.  Mongoose will queue up database
-// // operations and release them when the connection is complete.
-// mongoose.connect(uristring, function (err, res) {
-//   if (err) {
-//   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//   } else {
-//   console.log ('Succeeded connected to: ' + uristring);
-//   }
-// });
 
 var userSchema = new mongoose.Schema({
   email: String,
