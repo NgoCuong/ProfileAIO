50cd67e3314d21e2bcbd7862dc8286f8f897f273
ngocuong <cuonngo93@gmail.com>
2018-09-30 17:32:22 -0700

    hashed password register & login

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -51,24 +51,6 @@ router.post("/login", function (req, res) {
     var email = req.body.email;
     var password = req.body.password;
 
-    // findOne({ username: username }, function (err, user) {
-    //     if (err) deferred.reject(err.name + ': ' + err.message);
- 
-    //     if (user && bcrypt.compareSync(password, user.hash)) {
-    //         // authentication successful
-    //         deferred.resolve({
-    //             _id: user._id,
-    //             username: user.username,
-    //             firstName: user.firstName,
-    //             lastName: user.lastName,
-    //             token: jwt.sign({ sub: user._id }, config.secret)
-    //         });
-    //     } else {
-    //         // authentication failed
-    //         deferred.resolve();
-    //     }
-    // });
-
     User.findOne({ email: email }, function (err, user) {
         if (err) 
             deferred.reject(err.name + ': ' + err.message);
