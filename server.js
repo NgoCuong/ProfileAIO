4544d4d1683d6be64186fa614889ed5c314a05c7
ngocuong <cuonngo93@gmail.com>
2018-10-01 19:25:49 -0700

    backend authentication with passport

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var bodyParser = require("body-parser");
 var path = require('path');
 var app = express();
 var mongoose = require ("mongoose"); // The reason for this demo.
+var passport = require('passport');
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = require("./server/models/user.model");
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -21,6 +22,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/profile", funct
     console.log("Successfully connected to the database");
   }
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("express-session")({
+  secret: "team money",
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 // routes to api
 //var userRoutes = require("./server/routes/user");
