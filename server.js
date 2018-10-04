901570a02dee71dfb1848ba0e8df64b3c823a5ba
ngocuong <cuonngo93@gmail.com>
2018-10-04 16:33:11 -0700

    testing proocess.env

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require('path');
 var app = express();
-var mongoose = require ("mongoose"); // The reason for this demo.
+var mongoose = require ("mongoose"); 
 
 const jwt = require('express-jwt');
 const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
-// require('.env').config();
 
-// AUTH0_AUDIENCE="https://sicko-mode.auth0.com/api/v2/"
-// AUTH0_DOMAIN="sicko-mode.auth0.com"
-
-const corsOptions =  {
-  origin: 'http://localhost:4200'
-};
-app.use(cors(corsOptions));
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
-} else {
-  console.log("Found AUTH0 env");
-  console.log("AUTH0_DOMAIN: " + process.env.AUTH0_DOMAIN);
-  console.log("AUTH0_AUIDIENCE: " + process.env.AUTH0_AUDIENCE);
+  //For local
+  AUTH0_AUDIENCE= "https://sicko-mode.auth0.com/api/v2/";
+  AUTH0_DOMAIN= "sicko-mode.auth0.com";
+  AUTH0_JKSURI = `https://sicko-mode.auth0.com/.well-known/jwks.json`
 
+  const corsOptions =  {
+    origin: 'http://localhost:4200'
+  };
+  app.use(cors(corsOptions));
+} else {
+  //For Heroku
+  AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+  AUTH0_AUDIENCE  = `https://${process.env.AUTH0_DOMAIN}/`;
+  AUTH0_JKSURI = `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`;
 }
 
 const checkJwt = jwt({
@@ -33,13 +33,12 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://sicko-mode.auth0.com/.well-known/jwks.json`
+    jwksUri: AUTH0_JKSURI
   }),
 
   // Validate the audience and the issuer.
-  // audience: process.env.AUTH0_AUDIENCE,
-  aud: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  aud: AUTH0_AUDIENCE,
+  issuer: AUTH0_AUDIENCE,
   algorithms: ['RS256']
 });
 
