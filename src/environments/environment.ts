a662ca8ae4eb12d3daf4c3164de1a2cb5057c18f
ngocuong <cuonngo93@gmail.com>
2018-10-10 16:28:29 -0700

    update login component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   auth0: {
     clientId: '2NqEEV5hpcusvbL-gjRFe6u1FHZT6muE',
     domain: 'sicko-mode.auth0.com',
-    callbackURL: 'http://localhost:4200/callback',
+    callbackURL: 'https://nameless-hollows-54410.herokuapp.com/callback',
   },
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://nameless-hollows-54410.herokuapp.com"'
 };
