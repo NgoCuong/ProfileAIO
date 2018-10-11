e1fc8337e5ef97ca3d6333c388f3dd38f6be429a
ngocuong <cuonngo93@gmail.com>
2018-10-10 18:06:27 -0700

    fix pages height

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   constructor(public auth: AuthService) { }
   ngOnInit() {
     particlesJS.load('particles-js', '../../../assets/particlesjs-config.json', function() {
-      console.log('callback - particles.js config loaded');
+      // console.log('callback - particles.js config loaded');
     });
   }
 
