539e10c3f23462c8d61db07562570342850554c4
ngocuong <cuonngo93@gmail.com>
2018-10-14 23:48:16 -0700

    updated requests call

--- src/app/core/services/proxy.service.ts
+++ src/app/core/services/proxy.service.ts
@@ -10,13 +10,28 @@ export class ProxyService {
   constructor(private http: HttpClient) { }
 
   getProxy(param) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
     console.log(JSON.stringify(param));
-    this.http.post(environment.baseURL + '/api/proxy', JSON.stringify(param))
+    this.http.post(environment.baseURL + '/api/proxy', JSON.stringify(param), httpOptions)
       .subscribe(
         data => console.log(data),
         err => console.log(err)
       );
   }
+
+  /*
+  /** POST: add a new hero to the server */
+// addHero (hero: Hero): Observable<Hero> {
+//   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+//     tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+//     catchError(this.handleError<Hero>('addHero'))
+//   );
+// }
+
 
   deleteAll(param) {
     const url = environment.baseURL + '/api/proxy';
