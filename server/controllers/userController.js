e45cd0aa009d88875d96c326da9f70f1593163c3
ngocuong <cuonngo93@gmail.com>
2018-10-27 15:29:31 -0700

    refactored user API end pooints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,14 +4,12 @@ exports.getUser = (req, res) => {
   try {
     var userId = req.user.sub;
     if (typeof userId === "undefined") {
-      return res.status(403).json({msg: "User is not logged in." });
+      return res.status(403).json({ msg: "User is not logged in" });
     } else {
-      var userQuery = User.findOne({userId: userId});
-      userQuery.select('userId linodeKey proxyUsername proxyPassword googleUri');
-      userQuery.exec(function (err, user) {
-        if (err) {
+      User.findOne({userId: userId}, function(err, user) {
+        if(err) {
           console.log(`Failed getting user: ${userId}. Error: ${err}`);
-          return res.status(400).json({msg: 'Failed getting user settings'});
+          return res.status(400).json({ msg: 'Failed getting user settings' });
         } else {
           console.log(`Successfuly found user: ${userId}`);
           return res.status(200).json(user);
@@ -20,7 +18,7 @@ exports.getUser = (req, res) => {
     }
   } catch (err) {
     console.log(`Failed getting user. Error: ${err}`);
-    res.status(400).json({ msg: 'Failed getting user settings'});
+    res.status(400).json({ msg: 'Failed getting user settings' });
   }
 };
 
@@ -28,7 +26,7 @@ exports.updateUser = (req, res) => {
   try {
     var userId = req.user.sub;
     if (typeof userId === "undefined") {
-      res.status(400).json({ msg: "User is not logged in." });
+      res.status(403).json({ msg: "User is not logged in" });
     } else {
       User.findOneAndUpdate({ userId: userId }, {
         userName: req.body.userName == undefined ? "" : req.body.userName,
@@ -39,10 +37,32 @@ exports.updateUser = (req, res) => {
       }, { upsert: true }, function (err, suc) {
         if (err) {
           console.log(`Failed updating user:${userId}. Error: ${err}`);
-          res.status(400).json({ msg: err });
+          res.status(400).json({ msg: 'Failed updating user settings' });
         } else {
           console.log(`Successfully updating User's Setting. User: ${userId}`);
           res.status(200).json({ msg: "Successfully Updated" });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(`Failed updating user. Error: ${err}`);
+    res.status(400).json({ msg: err });
+  }
+};
+
+exports.deleteUser = (req, res) => {
+  try {
+    var userId = req.user.sub;
+    if (typeof userId === "undefined") {
+      res.status(403).json({ msg: "User is not logged in" });
+    } else {
+      User.deleteMany({ userId: userId }, function (err, suc) {
+        if (err) {
+          console.log(`Failed deleting user:${userId}. Error: ${err}`);
+          res.status(400).json({ msg: 'Failed deleteing user' });
+        } else {
+          console.log(`Successfully deleted User's Setting. User: ${userId}`);
+          res.status(200).json({ msg: "Successfully Deleted" });
         }
       });
     }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,86 +4,24 @@ var userController = require('../controllers/userController');
 
 router.route("/")
   .get(userController.getUser)
-  .post(userController.updateUser);
-
-// Delete a user from database
-router.delete("/", async function (req, res) {
-  try {
-    var userId = req.user.sub;
-
-    if (typeof userId === "undefined") {
-      return res.status(403).json({ msg: "Forbidden Access" });
-    } else {
-      var userSchema = require('../models/userModel');
-      var response = await userSchema.findOne({
-        'userId': userId
-      });
-
-      if (response != null) {
-        await response.delete();
-        return res.status(200).json({ msg: "Successfully Deleted" });
-      }
-      return res.status(400).json({ msg: "" });
-    }
-  } catch (err) {
-    return res.status(400).json(err);
-  }
-});
-
-// Updates one or more user property
-router.patch("/", async function (req, res) {
-  try {
-    var userId = req.user.sub;
-    if (typeof userId === "undefined") {
-      res.status(400).send({ msg: "userId must be defined" });
-    } else {
-
-      var userSchema = require('../models/userModel');
-      Object.assign(userSchema, req.body);
-      await userSchema.updateOne(
-        { 'userId': userId },
-        { $set: req.body },
-        function (err, rawResponse) {
-          if (err) throw err;
-          if (rawResponse.n == 0) {
-            res.status(400).json({ msg: "No values updated." });
-          } else {
-            res.status(200).json({ msg: "Values updated successfully" });
-          }
-        }
-      );
-    }
-  } catch (err) {
-    res.status(400).json({ msg: err });
-  }
-})
-
-
-// Updates all user properties
-router.put("/", function (req, res) {
-  try {
-    res.send("will be implemented");
-  } catch (err) {
-    res.send(400, err);
-  }
-});
-
+  .post(userController.updateUser)
+  .delete(userController.deleteUser);
 
 // Fetches all proxies on database
-router.get("/all/proxies", function (req, res) {
-  try {
-    var proxySchema = require('./proxySchema');
-    var query = proxySchema.find();
-    console.log(`Fetching all proxies`)
-    query.select('userId proxy region instanceId server');
-    query.exec(function (err, result) {
-      if (err) throw err;
-      res.status(200).json({ msg: result });
-    });
-  } catch (err) {
-    res.status(200).json({ msg: "Failed to fetch all proxies" });
-  }
-});
+// router.get("/all/proxies", function (req, res) {
+//   try {
+//     var proxySchema = require('./proxySchema');
+//     var query = proxySchema.find();
+//     console.log(`Fetching all proxies`)
+//     query.select('userId proxy region instanceId server');
+//     query.exec(function (err, result) {
+//       if (err) throw err;
+//       res.status(200).json({ msg: result });
+//     });
+//   } catch (err) {
+//     res.status(200).json({ msg: "Failed to fetch all proxies" });
+//   }
+// });
 
 
 module.exports = router;
